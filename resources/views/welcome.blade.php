118af5c8633887fc5fed5e0c4dae692370d47d8d
Nico Audy <nicoaudy1994@gmail.com>
2021-04-15 21:13:33 +0700

    link each page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
         <p class="lead text-muted">Producer of High Quality Food Products</p>
 		<p>With everything under one management, being responsible and reliable can come hand in hand with good quality and innovation. We are constantly innovating and conducting thorough research not only to drive profit and sustainability but also to promote us as a trusted brand for products that are Halal, Healthy, and Tasty.</p>
         <p>
-          <a href="#" class="btn btn-primary my-2">Belanja Sekarang</a>
+          <a href="/product" class="btn btn-primary my-2">Belanja Sekarang</a>
         </p>
       </div>
     </div>
